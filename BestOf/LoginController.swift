e9e1a49a10e1a5996e4414d3764098005de348ee
ericlee49 <ericlee49@gmail.com>
2016-09-17 00:54:17 -0700

    updated login register vc

--- BestOf/LoginController.swift
+++ BestOf/LoginController.swift
@@ -55,6 +55,36 @@ class LoginViewController: UIViewController {
         return textField
     }()
     
+    let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Email Address"
+        return textField
+    }()
+    
+    let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Password"
+        textField.secureTextEntry = true
+        return textField
+    }()
+    
+    
+    let nameToEmailDividerLine: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.lightGrayColor()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    
+    let emailToPasswordDividerLine: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.lightGrayColor()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     // MARK: Views setup & autolayouts
     
     func setupContainerView() {
@@ -70,11 +100,47 @@ class LoginViewController: UIViewController {
         // Add nameTextField to container & autolayout
         inputsViewContainerView.addSubview(nameTextField)
         // x, y, width, height constraints
-        nameTextField.centerXAnchor.constraintEqualToAnchor(inputsViewContainerView.centerXAnchor).active = true
+        nameTextField.leftAnchor.constraintEqualToAnchor(inputsViewContainerView.leftAnchor, constant: 12).active = true
         nameTextField.topAnchor.constraintEqualToAnchor(inputsViewContainerView.topAnchor).active = true
-        nameTextField.heightAnchor.constraintEqualToConstant((inputsViewContainerHeight / 3)).active = true
+        nameTextField.rightAnchor.constraintEqualToAnchor(inputsViewContainerView.rightAnchor, constant: -12).active = true
+        nameTextField.heightAnchor.constraintEqualToAnchor(inputsViewContainerView.heightAnchor, multiplier: 1/3).active = true
         
-        nameTextField.widthAnchor.constraintEqualToAnchor(inputsViewContainerView.widthAnchor).active = true
+        // Add emailTextField to container & autolayout
+        inputsViewContainerView.addSubview(emailTextField)
+        // x, y, width, height constraints
+        emailTextField.leftAnchor.constraintEqualToAnchor(inputsViewContainerView.leftAnchor, constant: 12).active = true
+        emailTextField.topAnchor.constraintEqualToAnchor(nameTextField.bottomAnchor).active = true
+        emailTextField.rightAnchor.constraintEqualToAnchor(inputsViewContainerView.rightAnchor, constant: -12).active = true
+        emailTextField.heightAnchor.constraintEqualToAnchor(inputsViewContainerView.heightAnchor, multiplier: 1/3).active = true
+        
+        // Add passwordTextField to container & autolayout
+        inputsViewContainerView.addSubview(passwordTextField)
+        // x, y, width, height constraints
+        passwordTextField.leftAnchor.constraintEqualToAnchor(inputsViewContainerView.leftAnchor, constant: 12).active = true
+        passwordTextField.topAnchor.constraintEqualToAnchor(emailTextField.bottomAnchor).active = true
+        passwordTextField.rightAnchor.constraintEqualToAnchor(inputsViewContainerView.rightAnchor, constant: -12).active = true
+        passwordTextField.heightAnchor.constraintEqualToAnchor(inputsViewContainerView.heightAnchor, multiplier: 1/3).active = true
+        
+        // Add nameToEmailDividerLine to container & autolayout
+        inputsViewContainerView.addSubview(nameToEmailDividerLine)
+        // x, y, width, height constraints
+        nameToEmailDividerLine.leftAnchor.constraintEqualToAnchor(inputsViewContainerView.leftAnchor).active = true
+        nameToEmailDividerLine.topAnchor.constraintEqualToAnchor(nameTextField.bottomAnchor).active = true
+        nameToEmailDividerLine.widthAnchor.constraintEqualToAnchor(inputsViewContainerView.widthAnchor).active = true
+        nameToEmailDividerLine.heightAnchor.constraintEqualToConstant(1).active = true
+        
+        // Add emailToPasswordDividerLine to container & autolayout
+        inputsViewContainerView.addSubview(emailToPasswordDividerLine)
+        // x, y, width, height constraints
+        emailToPasswordDividerLine.leftAnchor.constraintEqualToAnchor(inputsViewContainerView.leftAnchor).active = true
+        emailToPasswordDividerLine.topAnchor.constraintEqualToAnchor(emailTextField.bottomAnchor).active = true
+        emailToPasswordDividerLine.widthAnchor.constraintEqualToAnchor(inputsViewContainerView.widthAnchor).active = true
+        emailToPasswordDividerLine.heightAnchor.constraintEqualToConstant(1).active = true
+        
+
+
+        
+        
     }
     
     func setupLoginButton() {
